dfd6a01f89c5eb8ecd50c312b4b7494bb43006d6
rcolomina <rcolomina@gmail.com>
2023-05-13 12:18:09 +0100

    add main contract

---
+++ contracts/TradingSignalsArena.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract TradingSignalsArena {
+
+
+  //--------------------------------------------------------------------
+    /** ERRORS */
+
+  //--------------------------------------------------------------------
+    /** EVENTS */
+
+  //--------------------------------------------------------------------
+    /** FUNCTIONS */
+
+  function getTokenPrice(address token)
+        public
+        view
+        returns (uint256 tokenDaiPrice) {
+
+        tokenDaiPrice = supportedTokens[token].lastDaiPriceKnown;
+
+        // TODO: Add price feed oracle        
+        /*AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            supportedTokens[token].daiPriceFeed
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint256 decimals = priceFeed.decimals();
+        return uint256(price) / 10**decimals;*/
+    }
+
+
+  //--------------------------------------------------------------------
+    /** INTERNAL FUNCTIONS */
+
+
+  //--------------------------------------------------------------------
+    /** OWNER FUNCTIONS */
+
+}
